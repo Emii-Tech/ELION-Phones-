5d54bb039a139cce472adc6e2b7a575f1f5f01f4
Emeka Joseph <emiijoe1962@gmail.com>
2025-09-25 13:39:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+<script type="text/javascript">
+document.getElementById('contactButton').addEventListener('click', function() {
+  fbq('track', 'Contact');
+...
+}, false);
+</script>
